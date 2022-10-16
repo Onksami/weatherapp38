1dc76f2621684e454d82be195d3ee3133a6c2d2f
irfanonk <onk.irfan@gmail.com>
2022-10-16 17:04:19 +0300

    handle cities

--- app.js
+++ app.js
@@ -1,65 +1,101 @@
-const citySelect = document.getElementById('citySelect');
-const city = document.getElementById('city');
- 
-
+const citySelect = document.getElementById("citySelect");
+const city = document.getElementById("city");
+let cities = [];
 
 let date = new Date().toLocaleDateString("tr-TR");
 
 document.getElementById("date").innerHTML = date;
 
+let myRequest = new Request("./cities_of_turkey.json");
 
-
-let myRequest = new Request("./cities_of_turkey.json")
+// --- method 1 ----
 
 // fetch (myRequest).then (
 //     response => response.json())
 //    .then (data => {
 //     let cities = data.cities;
-    
+
 //     });
 
+// fetch(myRequest)
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // console.log(data);
+//     data.forEach((item) => {
+//       //   console.log("item", item);
+//       if (item.id < 30) {
+//         item.color = "red";
+//       } else if (item.id < 60) {
+//         item.color = "blue";
+//       } else {
+//         item.color = "purple";
+//       }
+//       let opt = document.createElement("option");
+//       opt.innerText = item.name;
+//       opt.value = JSON.stringify(item);
 
-fetch (myRequest).then (
-    response => {
-        return response.json()
-    }
-).then (data => {
+//       citySelect.appendChild(opt);
+//     });
+//     citySelect.addEventListener("change", onChangeCity);
+//   });
+
+// function onChangeCity(e) {
+//   const item = e.target.value;
+//   //   const name = e.target.selectedOptions[0].getAttribute("name");
+
+//   // citySelect[0] === target.innerText [0]
+//   console.log("e", item);
+
+//   city.innerText = item.name;
+//   city.style.color = item.color;
+// }
+
+// --- method 1 ----
+
+// --- method 2 ----
+
+fetch(myRequest)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    cities = data;
     // console.log(data);
-    data.forEach((item)=>{
-        // console.log(item.name);
-        let opt = document.createElement("option");
-        opt.innerText = item.name;
-        opt.value = item.id;
-        citySelect.appendChild(opt);
-        // console.log (item.name);
-        
-        
-      })
-      citySelect.addEventListener("change", asd)
-        
-});
+    data.forEach((item) => {
+      //   console.log("item", item);
 
-function asd(e) {
-    const filteredCity = e.target.value;
-    
+      let opt = document.createElement("option");
+      opt.innerText = item.name;
+      opt.value = item.id;
 
-    
-    // citySelect[0] === target.innerText [0]
-    console.log("e", filteredCity);
-   
-    
-    // const filteredCity = e.target.value;
-    // console.log("filteredCity", filteredCity);
-    // city.innerText = filteredCity;
-    
+      if (item.id < 30) {
+        opt.setAttribute("color", "red");
+      } else if (item.id < 60) {
+        opt.setAttribute("color", "blue");
+      } else {
+        opt.setAttribute("color", "purple");
+      }
+
+      citySelect.appendChild(opt);
+    });
+    citySelect.addEventListener("change", onChangeCity);
+  });
+
+function onChangeCity(e) {
+  const id = e.target.value;
+  const color = e.target.selectedOptions[0].getAttribute("color");
+
+  // citySelect[0] === target.innerText [0]
+  console.log("e", id, color);
+  console.log("cities", cities);
+
+  const filteredCity = cities.filter((city) => city.id == id);
+  console.log("filteredCity", filteredCity[0]);
+
+  city.innerText = filteredCity[0].name;
+  city.style.color = color;
 }
 
-
-
-
-
-
-
-    
-
-
+// --- method 1 ----
